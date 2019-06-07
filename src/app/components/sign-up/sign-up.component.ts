ef76d26d012fa3688644315a9f144bddfb719762
Emerson Price <emersonpriceiv@gmail.com>
2019-06-07 16:00:37 -0500

    Create the sign-in form with very simple validation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  signUpForm: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+    this.signUpForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.signUpForm.value);
+  }
+}
